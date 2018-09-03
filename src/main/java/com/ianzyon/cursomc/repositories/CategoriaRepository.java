09ac3d8c2f280ce03d4d576e7a5055b296018331
ianzyon <iansalgado@yahoo.com.br>
2018-09-03 15:25:41 -0300

    repository e service de categoria

---
+++ src/main/java/com/ianzyon/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.ianzyon.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ianzyon.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
