0237e83fe1e3eeb5f3f7fab1e362116732ed9a22
ianzyon <iansalgado@yahoo.com.br>
2018-09-03 18:08:59 -0300

    criando relacionamento Produto/Categoria no app e no db

--- src/main/java/com/ianzyon/cursomc/CursomcApplication.java
+++ src/main/java/com/ianzyon/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ianzyon.cursomc.domain.Categoria;
+import com.ianzyon.cursomc.domain.Produto;
 import com.ianzyon.cursomc.repositories.CategoriaRepository;
+import com.ianzyon.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+//	instancia a interface CategoriaRepository que esta fazendo a conexao JPA entre o DB e os objetos da aplicação
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,11 +27,25 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+//		Instanciações de novas categorias e produtos para ser passado ao DB
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+//		relaciona as categorias aos produtos, adicionando os produtos nas listas de produtos das categorias
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+//		relaciona os produtos as categorias, adicionando as categorias as listas de categorias dos produtos
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 		
 		
 		
--- src/main/java/com/ianzyon/cursomc/domain/Produto.java
+++ src/main/java/com/ianzyon/cursomc/domain/Produto.java
@@ -8,60 +8,94 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.ianzyon.cursomc.domain.Produto;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-	/**
-	 * 
-	 */
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
+	@ManyToMany
+	@JoinTable(
+			name = "PRODUTO_CATEGORIA",
+	joinColumns = @JoinColumn(name = "produto_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id")
+			)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Categoria() {
+	public Produto() {
 		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
+	/**
+	 * @return the id
+	 */
 	public Integer getId() {
 		return id;
 	}
 
+	/**
+	 * @param id the id to set
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return the nome
+	 */
 	public String getNome() {
 		return nome;
 	}
 
+	/**
+	 * @param nome the nome to set
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
 
-
-	public List<Produto> getProdutos() {
-		return produtos;
+	/**
+	 * @return the preco
+	 */
+	public Double getPreco() {
+		return preco;
 	}
 
-	
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	/**
+	 * @param preco the preco to set
+	 */
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	/**
+	 * @return the categorias
+	 */
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	/**
+	 * @param categorias the categorias to set
+	 */
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	/* (non-Javadoc)
@@ -86,7 +120,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -94,6 +128,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+	
+	
 	
 	
 }
